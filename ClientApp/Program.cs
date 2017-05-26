4eaf5f22d3be6c404d9ce076b98d3428caf06aae
高翔 <ross.gao@fairhr.com>
2017-05-26 09:16:22 +0800

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace ClientApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseUrls("http://localhost:8009")
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
